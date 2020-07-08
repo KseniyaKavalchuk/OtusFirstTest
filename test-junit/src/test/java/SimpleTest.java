c5a21c73da59a424a9c3660f6ee078c82e8053b5
Kseniya Kavalchuk <kseniyakavalchuk@Kseniyas-MacBook-Air.local>
2020-07-08 20:39:31 +0300

    Initial commit

---
+++ test-junit/src/test/java/SimpleTest.java
@@ -0,0 +1,36 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleTest {
+    protected static WebDriver driver;
+    private Logger logger = LogManager.getLogger(SimpleTest.class);
+
+    @Test
+    public void Log(){
+        logger.info("Info check");
+    }
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("Driver is up");
+    }
+
+    public void openPage() {
+        driver.get("https://otus.ru/");
+        logger.info("Otus page is opened");
+    }
+
+    public void setDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+        logger.info("Driver is closed");
+    }
+}
